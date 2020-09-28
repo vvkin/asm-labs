3348e157e96fe0dc675cd9549fbdadea5f74b0bd
Vadym Kinchur <vkincur@gmail.com>
2020-09-28 12:53:52 +0300

    Add lab2.asm

--- lab2/lab2.asm
+++ lab2/lab2.asm
@@ -1,64 +1,129 @@
-global _start         ; start point for linker
-
-section .data
-		maxLen equ 6      ; negative 16bit number (6 char's)
+global _start										; start point for linker
 
 section .bss
-		sign resb		1		  ; sign of got number (1 bit)
-		number resw	1	    ; input str after atoi (16 bit)
-		input  resb 6     ; input str (6 bit)
-		strLen resb 1     ; input length
+		buffer  resb   6						; input str (6 bit)
 
 section .text
 _start:
-		; get str from stdin
-		mov ebx, 2				; stdin
-		mov eax, 3				; sys_read
-		mov edx, maxLen   ; maximal length of input
-		mov ecx, input    ; place str to input variable
-		int 80h           ; call kernel
-
-_str_len:
-		xor eax, eax        ; result
-		mov ecx, input      ; put pointer to ecx
-
-_str_len_loop:
-		cmp [ecx], byte 0       ; check for \0
-		je _str_len_end
-		cmp [ecx], byte 10      ; check for \n
-		je _str_len_end
+		mov ebx, 2									; stdin
+		mov eax, 3									; sys_read
+		mov edx, 7									; maximal length of input
+		mov ecx, buffer							; place str to input variable
+		int 80h											; call kernel
 		
-		cmp byte [ecx], 45      ; it's minus
-		je _str_len_valid 
+		push buffer 
+		call _atoi									; result in ax
+		;pop [input]
 
-		cmp byte [ecx], 48      ; less than ord('0')
-		jl _str_len_error
-		cmp byte [ecx], 57      ; bigger than ord('9')
-		jg _str_len_error 
+		sub ax, 88							    ; operation (var. 10)
+		jo _start										; met overflow
+		
+		push ax
+		call _print_num
 
-_str_len_valid:
-		inc eax
-		add ecx, 1
-		jmp _str_len_loop
+_exit:	
+		mov ebx, 0
+		mov eax, 1
+		int 80h
+	  	
+;----atoi(const char* str) -> int (ax)
+;---- ax - output
+;---- bl - current char
+;---- dl - sign
+_atoi:
+		enter 0, 0
+		xor eax, eax
+		xor ebx, ebx
+		xor edx, edx
+		mov ecx, [ebp+8]						; pointer to first char in input
+		
+.loop:
+		mov bl, byte [ecx]					; current char
+		cmp bl, 0										; \0 
+		je .done
+		cmp bl, 10									; \n
+		je .done
 
-_str_len_error:
-		mov eax, -1
+		cmp bl, 45									; check for minus
+		je .check_sign
+		cmp bl, 48									; less than '0'
+		jl .error
+		cmp bl, 57									; greater than '9'
+		jg .error
 
-_str_len_end:
-		mov [strLen], eax  ; save len to variable
+.valid_digit:
+		sub bl, 48									; get digit
+		imul ax, word 10
+		jo _start
+		add ax, bx
+		jo _start
+		jmp .new_iteration
 
-_validate_input:
-		cmp [strLen], byte -1         ; got non numeric string
-		je _start
+.check_sign:
+		cmp ecx, [ebp+8]						; check is it first char
+		jne .error 
+		mov dl, 1
 
-_exit:
+.new_iteration:
+		inc ecx
+		jmp .loop
+
+.error:
+		leave                        ; error symbol
+		jmp _start									 ; prompt for new number
+
+.done:
+		cmp dl, 0										 ; check for sign
+		je .exit
+		neg ax
+		jo _start
+
+.exit:
+		leave
+		ret
+
+;-- print_num(int) -> void
+;-- print 16bit integer to stdin
+_print_num:
+		enter 0, 0
+		cmp word [ebp+8], 0          ; check sign 
+		jge .init
+		
+		push ecx                     ; save ecx
+		mov byte [buffer], 45
 		mov ebx, 1
 		mov eax, 4
-		mov edx, strLen
-		mov ecx, input
+		mov ecx, buffer              ; print '-' to stdout
+		mov edx, 1 
 		int 80h
+		pop ecx
 
-		mov ebx, 0
-	  mov eax, 1				; sys_exit
-	  int 80h
+		neg word [ebp+8]             ; make input positive
+
+.init:
+		mov ax, [ebp+8]
+		mov bx, 10
+		
+.loop:
+		xor dx, dx
+		div bx
+		add dl, 48 ; '0'
+		push dx
+		test ax, ax
+		jnz .loop
+		mov edi, ecx                 ; result will be in ecx 
+
+.stack:
+		pop ax	
+		stosb                        
+		cmp esp, ebp								 ; check stack is empty
+		jne .stack
+
+.print:
+		mov ebx, 1
+		mov eax, 4
+		mov edx, edi                 ; result length
+		int 80h
+		leave
+		ret
 
