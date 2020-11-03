96052ef1571d5f8c3931141b91dadacaf39a92a7
Vadym Kinchur <vkincur@gmail.com>
2020-11-03 22:11:15 +0200

    Rewrite few functions from lab3.asm with local variables

--- lab3/lab3.asm
+++ lab3/lab3.asm
@@ -1,25 +1,18 @@
 global _start
 
-%define BUFSIZE 256
+%define BUFF_SIZE 256
 
 section .data
     msgX db 'Enter x:', 10
     lenX equ $ - msgX
 
-    ten  dw 10
     fsig dq 0.0                 ; handle significant
     fexp dq 0.0                 ; exponent
 
 section .bss
-    buffer  resb BUFSIZE        ; buffer to read inpuy
+    buffer  resb BUFF_SIZE      ; buffer to read inpuy
     fbuffer resq 1              ; buffer to store double values
     ibuffer resd 1              ; buffer to store dword
-    sign    resb 1              ; number sign
-    dummy   resb 1              ; buffer to read garbage from stdin
-    valueX  resq 1              ; variable to handle x (float)
-    digit   resb 1
-    newcw   resw 1              ; control word
-    oldcw   resw 1
 
 section .text
 _start:
@@ -30,9 +23,13 @@ _start:
     mov edx, lenX
     mov ecx, msgX
     int 80h
-
-    call _read_to	
 		
+		mov ebx, 2
+		mov eax, 3
+		mov edx, BUFF_SIZE
+    mov ecx, buffer
+		int 80h
+
     push buffer
     call _atof
     add esp, 4
@@ -53,31 +50,6 @@ _exit:
     mov ebx, 0
     mov eax, 1                  ; sys_exit
     int 80h
-
-;--- read input from stdin to buffer 
-_read_to:
-    mov ecx, BUFSIZE     
-
-.clear_buffer:
-    mov byte [buffer+ecx], 0    ; fill with \0
-    loop .clear_buffer
-
-.read:
-    mov ebx, 2
-    mov edx, BUFSIZE
-    mov ecx, buffer
-
-.flush_stdin:
-    mov eax, 3                  ; sys_read
-    int 80h                     ; call kernel
-    cmp byte [ecx+eax-1], 10    ; compare last char in stdin with \n
-    je .exit
-    mov edx, 1
-    mov ecx, dummy
-    loop .flush_stdin
-
-.exit:
-    ret
 
 ;--- _pow10
 ;--- calculate 10 ^ n
@@ -102,13 +74,25 @@ _pow10:
 ;--- convert string to float
 ;--- output: st0
 _atof:
-    enter 0, 0
+    %define DIGIT byte [ebp-1]
+    %define SIGN  byte [ebp-2]
+		%define TEN   word [ebp-4] 
+		%define I_BUFFER dword [ebp-8]
+		%define F_BUFFER qword [ebp-16]
+
+    push ebp
+		mov ebp, esp
+		sub esp, 16
+
     fldz                        ; push 0 to st0
     xor eax, eax                ; number of digits after dot
     xor ebx, ebx        
     xor edx, edx                ; sign
     mov ecx, [ebp+8]            ; pointer to first char in input
-		
+	  
+		mov TEN, 10
+		mov SIGN, 0
+
 .loop:
     mov bl, byte [ecx]          ; current char
     cmp bl, 0                   ; \0 
@@ -126,15 +110,15 @@ _atof:
     jg .error
 
 .valid_digit:
-    add eax, edx                      ; ++number of digits after dot
+    add eax, edx                ; ++number of digits after dot
     sub bl, 48                  ; get digit
-    fimul word [ten]
-    mov [digit], bl
-    fiadd word [digit]
+    fimul TEN
+    mov DIGIT, bl
+    fiadd word DIGIT
     jmp .new_iteration
 
 .dot:                           ; if dot then nullify counter
-    test edx, edx                  ; check is it second dot
+    test edx, edx               ; check is it second dot
     jnz .error
     mov edx, 1
     jmp .new_iteration
@@ -142,7 +126,7 @@ _atof:
 .check_sign:
     cmp ecx, [ebp+8]            ; check is it first char
     jne .error 
-    mov byte [sign], 1
+    mov SIGN, 1
 
 .new_iteration:
     inc ecx
@@ -154,17 +138,17 @@ _atof:
     jmp _start                  ; prompt for a new number
 
 .done:
-    cmp byte [sign], 0          ; check for sign
+    cmp SIGN, 0                 ; check for sign
     je .divide
     fchs                        ; negate st0
 
 .divide:
-    fstp qword [fbuffer]        ; save st0
-    mov [ibuffer], eax
-    fild dword [ibuffer]        ; push eax to fstack
+    fstp F_BUFFER               ; save st0
+    mov I_BUFFER, eax
+    fild dword I_BUFFER         ; push eax to fstack
 
     call _pow10                 ; st0 = 10 ^ eax
-    fld qword [fbuffer]
+    fld qword F_BUFFER
     fdivrp
 
 .exit:
@@ -220,28 +204,42 @@ _calc:
 ;--- fexp - exp
 ;--- fsig - signficant
 _normalize:
-    ; fexp = floor(log_10(fvar))
-    fabs                         ; log doesn't support neg. values
-    fld st0
-    fldlg2                       ; st0 = log_10(2)
-    fxch st1                     ; st2=fvar, st1=log_10(2), st0=fvar
-    fyl2x                        ; st0 = log_10(2) * log_2(fvar)
-    fstcw [oldcw]                ; save previous rounding mode
-    mov dx, [oldcw]
-    or  dx, 0x0c000              ; rounding mode = 3, toward zero
-    mov [newcw], dx              ; new rounding mode
-    fldcw [newcw]                ; change rounding mode
-    frndint                      ; truncate log_10(input)
-    fldcw [oldcw]                ; restore rounding mode
-    fst qword [fexp]             ; store exp value
+    %define SIGN   word [ebp-2]
+    %define OLD_CW word [ebp-4]
+		%define NEW_CW word [ebp-6]
+
+		push ebp
+		mov esp, ebp
+		sub esp, 6
+    
+		; fexp = floor(log_10(fvar))
+		fld st0
+		fabs                        ; log doesn't support neg. values
+		fxch st1
+		fdiv st1, st0
+		fistp SIGN
+
+		fld st0
+    fldlg2                      ; st0 = log_10(2)
+    fxch st1                    ; st2=fvar, st1=log_10(2), st0=fvar
+    fyl2x                       ; st0 = log_10(2) * log_2(fvar)
+    fstcw OLD_CW                ; save previous rounding mode
+    mov dx, OLD_CW
+    or  dx, 0x0c000             ; rounding mode = 3, toward zero
+    mov NEW_CW, dx              ; new rounding mode
+    fldcw NEW_CW                ; change rounding mode
+    frndint                     ; truncate log_10(input)
+    fldcw OLD_CW                ; restore rounding mode
+    fst qword [fexp]            ; store exp value
 
     ; fsig = fvar / 10^(fexp)
-    fxch st1                     ; st0=fvar, st1=fexp
-    fstp qword [fbuffer]         ; fbuffer=fvar, st0=fexp
-    call _pow10                  ; st0=10^fexp
-    fld qword [fbuffer]          ; st0=fvar, st1=10^fexp
-    fdivrp                       ; st0=fvar/10^fexp
-    cmp byte [sign], 0
+    fxch st1                    ; st0=fvar, st1=fexp
+    fstp qword [fbuffer]        ; fbuffer=fvar, st0=fexp
+    call _pow10                 ; st0=10^fexp
+    fld qword [fbuffer]         ; st0=fvar, st1=10^fexp
+    fdivrp                      ; st0=fvar/10^fexp
+    
+		cmp SIGN, 0
     je .exit
     fchs
 
@@ -284,59 +282,59 @@ _dtoa:
 
 ; isolate digits of fractional part and store ASCII
 .get_fractional:
-    fmul st0, st1               ; Multiply by 10 (shift one decimal digit into integer part)
-    fist word TEMP              ; Store digit
-    fisub word TEMP             ; Clear integer part
-    mov al, byte TEMP           ; Load digit
-    or al, 0x30                 ; Convert digit to ASCII
-    mov byte [edi], al          ; Append it to string
-    add edi, 1                  ; Increment pointer to string
-    fxam                        ; ST0 == 0.0?
+    fmul st0, st1               ; multiply by 10 (shift one decimal digit into integer part)
+    fist word TEMP              ; store digit
+    fisub word TEMP             ; clear integer part
+    mov al, byte TEMP           ; load digit
+    or al, 0x30                 ; convert digit to ASCII
+    mov byte [edi], al          ; append it to string
+    add edi, 1                  ; increment pointer to string
+    fxam                        ; st0 == 0.0?
     fstsw ax
     sahf
-    jnz .get_fractional         ; No: once more
+    jnz .get_fractional         ; no: once more
     mov byte [edi], 0           ; Null-termination for ASCIIZ
 
     ; clean up FPU
-    ffree st0                   ; Empty ST(0)
-    ffree st1                   ; Empty ST(1)
-    fldcw CONTROL_WORD          ; Restore old rounding mode
+    ffree st0                   ; empty ST(0)
+    ffree st1                   ; empty ST(1)
+    fldcw CONTROL_WORD          ; restore old rounding mode
 
     leave
-    ret                             ; Return: EDI points to the null-termination of the string
+    ret                         ; return: edi points to the null-termination of the string
 
-fpu2bcd2dec:                    ; Args: ST(0): FPU-register to convert, EDI: target string
+fpu2bcd2dec:                    ; args: st0: FPU-register to convert, edi: target string
     push ebp
     mov ebp, esp
     sub esp, 10                 ; 10 bytes for local tbyte variable
 
     fbstp [ebp-10]
 
-    mov ecx, 10                 ; Loop counter
+    mov ecx, 10                 ; loop counter
     lea esi, [ebp - 1]          ; bcd + 9 (last byte)
-    xor bl, bl                  ; Checker for leading zeros
+    xor bl, bl                  ; checker for leading zeros
 
-    ; Handle sign
-    btr word [ebp-2], 15        ; Move sign bit into carry flag and clear it
-    jnc .L1                     ; Negative?
-    mov byte [edi], '-'         ; Yes: store a minus character
+    ; handle sign
+    btr word [ebp-2], 15        ; move sign bit into carry flag and clear it
+    jnc .L1                     ; negative?
+    mov byte [edi], '-'         ; yes: store a minus character
     add edi, 1
 
 .L1:
     mov al, byte [esi]
     mov ah, al
-    shr ah, 4               ; Isolate left nibble
-    or bl, ah               ; Check for leading zero
+    shr ah, 4                   ; isolate left nibble
+    or bl, ah                   ; check for leading zero
     jz .1
-    or ah, 30h              ; Convert digit to ASCII
+    or ah, 30h                  ; convert digit to ASCII
     mov [edi], ah
     add edi, 1
     
 .1:
-    and al, 0Fh             ; Isolate right nibble
-    or bl, al               ; Check for leading zero
+    and al, 0Fh                 ; isolate right nibble
+    or bl, al                   ; check for leading zero
     jz .2
-    or al, 30h              ; Convert digit to ASCII
+    or al, 30h                  ; convert digit to ASCII
     mov [edi], al
     add edi, 1
     
@@ -344,12 +342,12 @@ fpu2bcd2dec:                    ; Args: ST(0): FPU-register to convert, EDI: tar
     sub esi, 1
     loop .L1
 
-    test bl, bl                 ; BL remains 0 if all digits were 0
-    jnz .R1                     ; Skip next line if integral part > 0
+    test bl, bl                 ; bl remains 0 if all digits were 0
+    jnz .R1                     ; skip next line if integral part > 0
     mov byte [edi], '0'
     add edi, 1
 
 .R1:
-    mov byte [edi], 0           ; Null-termination for ASCIIZ
+    mov byte [edi], 0           ; null-termination for ASCIIZ
     leave
-    ret                         ; Return: EDI points to the null-termination of the string
+    ret                         ; return: EDI points to the null-termination of the string
