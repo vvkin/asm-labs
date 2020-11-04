70e21408a16f4ff8fcb01a141c24a21cce0f4858
D_MENT <44676701+rocket111185@users.noreply.github.com>
2020-11-04 14:16:39 +0200

    Simplify build.sh argument input
    Instead of "labN", just input "N"

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 # usage: chmod +x build.sh
-#        ./build.sh [dir_name]
+#        ./build.sh [lab_number]
 
-dir_name="$PWD/$1"
+labname="lab$1"
+dir_name="$PWD/$labname"
 
-nasm -g -f elf -l "$dir_name/$1.lst" "$dir_name/$1.asm"
-ld -m elf_i386 -o "$dir_name/$1" "$dir_name/$1.o"
+nasm -g -f elf -l "$dir_name/$labname.lst" "$dir_name/$labname.asm"
+ld -m elf_i386 -o "$dir_name/$labname" "$dir_name/$labname.o"
